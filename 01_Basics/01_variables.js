cd26bbe30a99f76f4f5b83fe436d78d08fb55121
RIDISA SAHA <ananadadayini108@gmail.com>
2024-11-07 18:57:13 +0530

    Learning javascript Day 1 {Hare Krishna... Gauranga...!!}

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = "123468910"
+let accountEmail = "anandadayini108@gmail.com"
+var password = "Harekrishna108"
+accountCity = "Mayapur"
+let accountState;
+console.table([accountEmail, accountCity,accountId, password, accountState])
+
+
